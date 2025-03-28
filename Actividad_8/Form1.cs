a35b6cf27c235296ee75b1aeafea5e914354bcd3
PaolaJAntunez <antunez.duarte20@gmail.com>
2025-03-28 12:31:21 -0600

    Add Actividad_8/Form1.cs 1743186681351

---
+++ Actividad_8/Form1.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Actividad_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void controlDeEmpleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 formEmpleados = new Form2();
+            formEmpleados.MdiParent = this;
+            formEmpleados.Show();
+        }
+
+        private void vistaGeneralToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 formVista = new Form3();
+            formVista.MdiParent = this;
+            formVista.Show();
+        }
+
+        private void pagosEmpleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form4 formPagos = new Form4();
+            formPagos.MdiParent = this;
+            formPagos.Show();
+        }
+
+        private void calculadoraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("calc.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir la calculadora: " + ex.Message);
+            }
+        }
+
+        private void blocNotasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("notepad.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el bloc de notas: " + ex.Message);
+            }
+        }
+
+        private void paginaWebUSAPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("https://www.usap.edu/");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir la página web: " + ex.Message);
+            }
+        }
+
+        private void creditosDelSistemaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "UNIVERSIDAD DE SAN PEDRO SULA - USAP\n" +
+                "PROGRAMACION 1 / DMV (1P-2025)\n" +
+                "ELABORADO POR KATHERINE QUINTANILLA (2240056)",
+                "Créditos del sistema",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Está seguro que desea salir?", "Confirmar", 
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
